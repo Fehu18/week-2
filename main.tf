0eb12288b6d1c734ddc725d4d172224a41eb1412
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-06-26 13:34:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -70,15 +70,7 @@ resource "azurerm_key_vault" "kv" {
     object_id = data.azurerm_client_config.current.object_id
 
     key_permissions = [
-      "Get","Create"
-    ]
-
-    secret_permissions = [
-      "Get",
-    ]
-
-    storage_permissions = [
-      "Get",
+      "Get","Create","SetRotationPolicy","Rotate","Decrypt","Encrypt","GetRotationPolicy"
     ]
   }
 }
