ffbb181ab8dc8139a0e9459c9196a4dbc8dfd15a
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-06-26 12:02:32 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -45,7 +45,7 @@ variable "vm_name" {
 
 variable "vm_size" {
   description = "The size of the virtual machine"
-  default     = "Standard_DS1_v2"
+  default     = "Standard_B2s"
 }
 
 variable "admin_username" {
